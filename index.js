d4a9861a46a917c690e709f023e0b87266d94aec
Robert Cordes <robert@diverently.com>
2022-01-07 19:58:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,38 +12,38 @@ var assign = require('object-assign'); // fixme: remove after postcss node 0.12 
 
 //
 // RGB(a) regex
-var rgbReg = /rgb\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+)?\)/g;
-var rgbaReg = /rgba\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+|,\s*var\(--tw-text-opacity\))?\)/g;
-var rgbRgbaReg = /rgba?\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+)?\)/g;
+var rgbReg = /rgb\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*?\)/g;
+var rgbaReg = /rgba\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+|,\s*var\(--tw-(?:.*)-opacity\))?\)/g;
+var rgbRgbaReg = /rgba?\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+|,\s*var\(--tw-(?:.*)-opacity\))?\)/g;
 
 /**
  * PostCSS plugin
  * @type {*}
  */
-module.exports = postcss.plugin('postcss-rgba-hex', function(options) {
+module.exports = postcss.plugin('postcss-rgba-hex', function (options) {
 
-    var reg = rgbRgbaReg;
+    var reg = rgbaReg;
     var o = assign({}, options);
 
-    if(o.rgbOnly && o.rgbaOnly) {
+    if (o.rgbOnly && o.rgbaOnly) {
         console.error('Invalid options');
         return noop;
     }
 
-    if(o.rgbOnly) {
+    if (o.rgbOnly) {
         reg = rgbReg;
     }
 
-    if(o.rgbaOnly) {
+    if (o.rgbaOnly) {
         reg = rgbaReg;
     }
 
-    return function(style) {
-        style.walkDecls(function(decl) {
+    return function (style) {
+        style.walkDecls(function (decl) {
             var val = decl.value;
 
             // early return
-            if(!val) {
+            if (!val) {
                 return;
             }
 
@@ -51,14 +51,15 @@ module.exports = postcss.plugin('postcss-rgba-hex', function(options) {
             var rgbValues = val.match(reg);
 
             // converting values
-            if(rgbValues && rgbValues.length > 0) {
+            if (rgbValues && rgbValues.length > 0) {
                 var newVal = val;
 
-                rgbValues.forEach(function(rgb) {
-                    newVal = newVal.replace(rgb, rgbaToHex(rgb));
+                rgbValues.forEach(function (rgb) {
+                    var rgbString = rgb.replace(/,\s*var\(--tw-(.*)-opacity\)/, '');
+                    newVal = newVal.replace(rgbString, rgbaToHex(rgbString));
 
                     if (!o.silent) {
-                        console.info('RGB(a) replaced: ' + rgb + ' -> ' + rgbaToHex(rgb));
+                        console.info('RGB(a) replaced: ' + rgb + ' -> ' + rgbaToHex(rgbString));
                     }
                 });
                 decl.value = newVal;
